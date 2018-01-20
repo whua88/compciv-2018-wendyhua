346a63666a0db4bdddab6d2a65c3d904a868ab6b
Wendy Hua <wendyhua@stanford.edu>
2018-01-19 23:47:59 -0800

    latest

--- week-02/foozbuzz/fooz.py
+++ week-02/foozbuzz/fooz.py
@@ -1,2 +1,10 @@
-def fob():
-    print('blasjfdksldfj')
+def fob(y):
+    for i in range(1, y):
+        if i % 3 is 0 and i % 5 is 0:
+            print(i, '', 'FizzBuzz')
+        elif i % 3 is 0:
+            print(i, '', 'Fizz')
+        elif i % 5 is 0:
+            print(i, '', 'Buzz')
+        else:
+            print(i)
